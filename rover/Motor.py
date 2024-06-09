636ac9a8c249b3428c842aea9097c32064e42322
Cric_Hammel <cric_hammel@gmx.de>
2024-06-09 02:48:53 -0300

    Removed unnecessary self calls + formatting changes

--- rover/Motor.py
+++ rover/Motor.py
@@ -3,36 +3,36 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 class Motor:
-    def __init__(self,PWMA_pin , AIN2_pin, AIN1_pin, STBY_pin):
+    def __init__(PWMA_pin, AIN2_pin, AIN1_pin, STBY_pin):
         self.PWMA_pin = PWMA_pin
-        self.AIN2_pin = AIN2_pin
         self.AIN1_pin = AIN1_pin
+        self.AIN2_pin = AIN2_pin
         self.STBY_pin = STBY_pin
         
         GPIO.setup(PWMA_pin, GPIO.OUT)
-        GPIO.setup(AIN2_pin, GPIO.OUT)
         GPIO.setup(AIN1_pin, GPIO.OUT)
+        GPIO.setup(AIN2_pin, GPIO.OUT)
         GPIO.setup(STBY_pin, GPIO.OUT)
         
-    def foward(self):
+    def foward():
         GPIO.output(AIN1_pin, GPIO.HIGH)
         GPIO.output(AIN2_pin, GPIO.LOW)
 
-    def backwards(self):
-        GPIO.output(AIN1_pin, GPIO.Low)
+    def backwards():
+        GPIO.output(AIN1_pin, GPIO.LOW)
         GPIO.output(AIN2_pin, GPIO.HIGH)
 
-    def set_speed(self):
+    def set_speed():
         GPIO.output(PWMA_pin, GPIO.HIGH)
         
-    def start(self):
-        GPIO.output (STBY_pin, GPIO.HIGH)
+    def start():
+        GPIO.output(STBY_pin, GPIO.HIGH)
 
-    def stop(self):
-        GPIO.output (STBY_pin, GPIO.LOW)
+    def stop():
+        GPIO.output(STBY_pin, GPIO.LOW)
 
-    def GPIO_reset(self):
+    def GPIO_reset():
         GPIO.output(PWMA_pin, GPIO.LOW)
-        GPIO.output(AIN2_pin, GPIO.LOW)
         GPIO.output(AIN1_pin, GPIO.LOW)
+        GPIO.output(AIN2_pin, GPIO.LOW)
         GPIO.output(STBY_pin, GPIO.LOW)
